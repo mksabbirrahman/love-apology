b8d2ee953cbae45e56561ff23ef0c0a2d8573ea1
Sabbir Hossen <66940849+mksabbirrahman@users.noreply.github.com>
2026-01-06 23:58:29 +0600

    Add fillLove and forgive functions in script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function fillLove() {
+  document.getElementById("fill").style.width = "100%";
+}
+
+function forgive() {
+  document.getElementById("message").innerHTML =
+    "Thank you for still choosing me. I love you endlessly ❤️";
+}
