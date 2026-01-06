f6508bf4a626c2fee3253086e6c40636aa3d8065
Sabbir Hossen <66940849+mksabbirrahman@users.noreply.github.com>
2026-01-07 00:11:41 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function fillLove() {
   document.getElementById("fill").style.width = "100%";
-
-  if (navigator.vibrate) {
-    navigator.vibrate(200);
-  }
+  if (navigator.vibrate) navigator.vibrate(200);
 }
 
 function forgive() {
   document.getElementById("message").innerHTML =
-    "তবুও আমাকে বেছে নেওয়ার জন্য ধন্যবাদ। আমি তোমাকে সীমাহীন ভালোবাসি ❤️";
+    "তোমার একটুখানি হাসিই আমার সব রাগ ভুলিয়ে দেয়। আমি তোমাকে ভালোবাসি ❤️";
+}
+
+function playMusic() {
+  const music = document.getElementById("bgMusic");
+  music.play();
 }
